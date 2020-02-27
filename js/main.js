1c2aaf04251f28f73107994028497963209a66fa
Roman Torres <rtorres@envivent.com>
2020-02-27 12:19:04 -0800

    set interval to cycle through array and reset

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
 class Slideshow {
     constructor(el){
-
+        // this.el = document.querySelector(el);
+        this.slides = [0, 1, 2, 3, 4, 5];
+        this.current = 0;
+        let time = 1000;
+        // console.log(this.el);
+ 
+        setInterval(() => {
+            console.log(this.current);
+            this.next();
+        }, time);
     }
 
     next() {
+        // console.log(this.el);
+        if (this.current >= this.slides.length -1) {
+            this.goto(0);
+            return
+        }
 
+        this.goto(++this.current);
     }
 
     prev() {
-
+        if (this.current <= 0) {
+            this.current = this.slides.length;
+        }
+        
+        this.slides[--this.current];
     }
 
     goto(i) {
-
+        this.current = i;
+        this.slides[i];
     }
 
     pause() {
@@ -24,4 +44,18 @@ class Slideshow {
     }
 }
 
-let ss = new Slideshow();
+let ss = new Slideshow(".pictures");
+
+// let slides = [0, 1, 2, 3, 4, 5];
+// let current = 0;
+// let time = 1000;
+
+// setInterval(() => {
+//     console.log(current);
+//     if (current >= slides.length -1) {
+//         current = -1;
+//     }
+
+//     slides[current++];
+
+// }, time);
